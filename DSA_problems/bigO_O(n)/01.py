b824ce144d3fe976e9ecedab82de5a1f08f2b931
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-11-04 13:32:19 -0500

    Initial commit

---
+++ DSA_problems/bigO_O(n)/01.py
@@ -0,0 +1,38 @@
+#Write a function that prints all elements in an array.
+def all_elements():
+    l_elements=[]
+    n=int(input("How many elements do you wanna enter?"))
+    for i in range(n):
+        ele=input("Enter a element")
+        l_elements.append(ele)
+    print(l_elements)
+all_elements()
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def all_element():
+    all_ele=[]
+    n= int(input("How many elements do you wanna enter? "))
+    for i in range(n):
+        e=input("Enter the element: ")
+        all_ele.append(e)
+    print(all_ele)
+
+all_element()

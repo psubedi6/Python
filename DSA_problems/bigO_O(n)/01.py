46f06549b2bcedf1eedcf4393606ea4d5759c7af
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-11-04 14:15:23 -0500

    01.py-Write a function that prints all elements in an array.

--- DSA_problems/bigO_O(n)/01.py
+++ DSA_problems/bigO_O(n)/01.py
@@ -1,38 +1,22 @@
 #Write a function that prints all elements in an array.
+#(using while loop)
+def all_elements():
+    my_list=[]
+    while True:
+        a=input("Enter a element: ")
+        my_list.append(a)
+        continuee=input("Do you wanna continue? ")
+        if continuee != "yes":
+            break
+    print(my_list)
+all_elements()
+
+#(using for loop)
 def all_elements():
     l_elements=[]
-    n=int(input("How many elements do you wanna enter?"))
+    n=int(input("How many elements do you wanna enter? "))
     for i in range(n):
-        ele=input("Enter a element")
+        ele=input("Enter a element: ")
         l_elements.append(ele)
     print(l_elements)
 all_elements()
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def all_element():
-    all_ele=[]
-    n= int(input("How many elements do you wanna enter? "))
-    for i in range(n):
-        e=input("Enter the element: ")
-        all_ele.append(e)
-    print(all_ele)
-
-all_element()

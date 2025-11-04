789df810b96d1398ea15c6feafeab4fdc8230889
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-11-04 13:53:18 -0500

    Fixed string folder

---
+++ Strings/Levels/Level_3/05.py
@@ -0,0 +1,3 @@
+#Align the string "Python" to the center in a field of width 20 using str.center()
+a="Python"
+print(a.center(20))
---
+++ Strings/Levels/Level_3/07.py
@@ -0,0 +1,3 @@
+#Replace all occurrences of "is" with "was" in "This is Python. It is fun".
+a= "This is Python. It is fun"
+print(a.replace("is", "was"))

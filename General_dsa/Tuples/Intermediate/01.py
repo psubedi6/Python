ce3fbf16d4dd742f55cfd168dba2f81d2b804a53
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-11-10 21:22:22 -0500

    01.py-Given two tuples (1, 2, 3) and (4, 5, 6), concatenate them and repeat the new tuple 3 times.

---
+++ General_dsa/Tuples/Intermediate/01.py
@@ -0,0 +1,6 @@
+#Given two tuples (1, 2, 3) and (4, 5, 6), concatenate them and repeat the new tuple 3 times.
+tuple1=(1,2,3)
+tuple2= (4,5,6)
+newtuple= tuple1 + tuple2
+for i in range(0,3):
+    print(newtuple)

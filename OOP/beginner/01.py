452dff04906ec3117cf16702620347acac9899b6
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-11-30 15:30:48 -0500

    01.py- define a Person class with name, age and a greet() method that prints a greeting. Instantiate and call it.

---
+++ OOP/beginner/01.py
@@ -0,0 +1,12 @@
+#Define a Person class with name, age and a greet() method that prints a greeting. Instantiate and call it.
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello {self.name}, You are {self.age} years old")
+
+p1= Person("Prakash", "24")
+p1.greet()
